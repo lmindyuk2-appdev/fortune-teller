0a8b6fe157e249bc45a464cc0cab60bc08d5cf60
lmindyuk2 <lmindyuk@chicagobooth.edu>
2020-07-21 23:56:56 +0000

    horoscope done

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -7,7 +7,18 @@ class NumbersController < ApplicationController
       
       @zebra.push(giraffe)
     end
-
     render("lottery_stuff/woohoo.html.erb")
+  end 
+
+  def loosers
+      @zebra = Array.new
+
+    5.times do
+      giraffe = rand(1...100)
+      
+      @zebra.push(giraffe)
+    end
+
+    render("lottery_stuff/ohno.html.erb")
   end 
 end
